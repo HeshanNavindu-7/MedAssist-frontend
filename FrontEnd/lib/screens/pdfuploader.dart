3d1d1658117101a9c938bf6313023ae2352ed2a1
D-Binara <dbkaveesha@gmail.com>
2024-07-16 00:28:34 +0530

    change ports

--- FrontEnd/lib/screens/pdfuploader.dart
+++ FrontEnd/lib/screens/pdfuploader.dart
@@ -24,7 +24,7 @@ class _PdfUploader extends State<PdfUploader> {
   }
 
   Future<void> _fetchUserId() async {
-    final response = await http.get(Uri.parse('http://10.0.2.2:8000/users/'));
+    final response = await http.get(Uri.parse('http://192.168.8.102:8000/users/'));
 
     if (response.statusCode == 200) {
       // Parse the response JSON
@@ -57,7 +57,7 @@ class _PdfUploader extends State<PdfUploader> {
 
           var request = http.MultipartRequest(
             'POST',
-            Uri.parse('http://10.0.2.2:8000/upload_pdf/'),
+            Uri.parse('http://192.168.8.102:8000/upload_pdf/'),
           );
 
           request.files.add(
--- FrontEnd/lib/screens/secondPage.dart
+++ FrontEnd/lib/screens/secondPage.dart
@@ -17,11 +17,12 @@ class SecondPage extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 350),
-              child: Padding(
+        child: SingleChildScrollView( 
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 350),
+                child: Padding(
                   padding: EdgeInsets.only(left: 40.0, right: 40.0),
                   child: Column(
                     children: [
@@ -45,72 +46,73 @@ class SecondPage extends StatelessWidget {
                           ))
                     ],
                   )),
-            ),
-            const SizedBox(
-              width: 60,
-              height: 90,
-            ),
-            Align(
-              alignment:
-              const Alignment(0, 0.7), // Aligning to the desired position
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignInPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                    ),
-                    child: const Text(
-                      'Sign In',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 60),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignUp_Page(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF004080),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                    ),
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ],
               ),
-            ),
-          ],
+              const SizedBox(
+                width: 60,
+                height: 90,
+              ),
+              Align(
+                alignment:
+                const Alignment(0, 0.7), // Aligning to the desired position
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignInPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                      ),
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 60),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignUp_Page(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF004080),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                      ),
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
