c347464a40e1e1aceface84c568a075a4298af9e
Heshan Navindu <121144605+HeshanNavindu-7@users.noreply.github.com>
2024-07-22 12:55:27 +0530

    jk

--- FrontEnd/lib/APIs/userDetails.dart
+++ FrontEnd/lib/APIs/userDetails.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class UserDataManager {
   static Future<Map<String, dynamic>> fetchUserDetails() async {
-    final response = await http.get(Uri.parse('http://192.168.8.102:8000/users/'));
+    final response = await http.get(Uri.parse('http://10.0.2.2:8000/users/'));
 
     if (response.statusCode == 200) {
       // Parse the response JSON
